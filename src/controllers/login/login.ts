859390313cd6f6c2beaa1f2aa7d0a42bdaab4d68
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-10 17:37:38 -0300

    fix: correct lint

--- src/controllers/login/login.ts
+++ src/controllers/login/login.ts
@@ -1,18 +1,18 @@
 export default class LoginController {
-  handle(httpRequest: any): any {
+  handle (httpRequest: any): any {
     const httpResponse = {
       statusCode: 400,
-      body: new Error(),
-    };
+      body: new Error()
+    }
 
     if (!httpRequest.body.email) {
-      httpResponse.body = new Error('Missing param: email');
+      httpResponse.body = new Error('Missing param: email')
     }
 
     if (!httpRequest.body.password) {
-      httpResponse.body = new Error('Missing param: password');
+      httpResponse.body = new Error('Missing param: password')
     }
 
-    return httpResponse;
+    return httpResponse
   }
 }
