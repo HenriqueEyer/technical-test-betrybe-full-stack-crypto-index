a2941db40a121a1c75ba78d757184d9c4059a837
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-10 19:05:46 -0300

    refactor: create interface for controller

--- src/controllers/login/login.ts
+++ src/controllers/login/login.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../interfaces/http'
 import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from 'controllers/interfaces/controller'
 
-export default class LoginController {
+export default class LoginController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['email', 'password']
     for (const field of requiredFields) {
