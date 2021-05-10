abb086c3309e7ba99c5bc3f43ac3b085d585ff2b
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-10 16:37:54 -0300

    test: ensure LoginController return a erro if no email or password is provided

---
+++ src/controllers/login/login.spec.ts
@@ -0,0 +1,27 @@
+import LoginController from './login';
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginController();
+    const httpRequest = {
+      body: {
+        password: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  });
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginController();
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: password'))
+  });
+});
