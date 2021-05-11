4fa7a8f9c89b261e9175d6ac3f3da0940c9acdfb
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-11 13:47:18 -0300

    feat: ensure PasswordValidatorAdapter expect work

---
+++ src/services/validators/password-validator.ts
@@ -0,0 +1,10 @@
+import { PasswordValidator } from '../../controllers/interfaces'
+import validator from 'validator'
+
+export class PasswordValidatorAdapter implements PasswordValidator {
+  isValid (password: string): boolean {
+    const isNumber = validator.isNumeric(password)
+    const isCorrectSize = password.length === 6
+    return isNumber && isCorrectSize
+  }
+}
