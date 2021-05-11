3e72824e36a85143ee61ab30cc7ccce9be9757c0
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-11 16:59:19 -0300

    test: ensure TokenServiceAdpater.validToken return a true if utils return true

--- src/services/token/token-service.spec.ts
+++ src/services/token/token-service.spec.ts
@@ -2,23 +2,30 @@ import { TokenServiceAdapter } from './token-service'
 import validator from 'validator'
 
 describe('TokenService', () => {
-  test('Should TokenService return a token with size 16', () => {
+  test('Should generateToken return a token with size 16', () => {
     const sut = new TokenServiceAdapter()
     const token = sut.generateToken()
     expect(token.length).toBe(16)
   })
 
-  test('Should TokenService return a token just with Alfanumeric', () => {
+  test('Should generateToken return a token just with Alfanumeric', () => {
     const sut = new TokenServiceAdapter()
     const token = sut.generateToken()
     const isAphaNumeric = validator.isAlphanumeric(token)
     expect(isAphaNumeric).toBe(true)
   })
 
-  test('Should TokenService return a token different token with another login was done', () => {
+  test('Should generateToken return a token different token with another login was done', () => {
     const sut = new TokenServiceAdapter()
     const token1 = sut.generateToken()
     const token2 = sut.generateToken()
     expect(token1).not.toBe(token2)
+  })
+
+  test('Should validToken return true if utils validation return true', () => {
+    const sut = new TokenServiceAdapter()
+    const token = 'any_token'
+    const isValid = sut.validToken(token)
+    expect(isValid).toBe(true)
   })
 })
