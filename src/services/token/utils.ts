429720aaf98be44516bed6ac8d793ba24a3c64a0
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-11 16:48:49 -0300

    feat: add validation in util

--- src/services/token/utils.ts
+++ src/services/token/utils.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import { promises } from 'fs'
+import validator from 'validator'
 
 const getTokens = async (): Promise<string[]> => {
   const content = await promises.readFile(path.resolve(__dirname, 'data', 'tokens.json'))
@@ -7,8 +8,16 @@ const getTokens = async (): Promise<string[]> => {
 }
 
 export const isValidToken = async (token: string): Promise<boolean> => {
+  const isValidFormat = validator.isAlphanumeric(token) && token.length === 16
+  if (!isValidFormat) {
+    return false
+  }
   const tokens = await getTokens()
-  return tokens.some(value => value === token)
+  const isGenerateToken = tokens.some(value => value === token)
+  if (!isGenerateToken) {
+    return false
+  }
+  return true
 }
 
 export const generateToken = (): string => `${Math.random().toString(36).slice(-10)}${Math.random().toString(36).slice(-6)}`
