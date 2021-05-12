78f112166f1cc5334fa6ce3a57aac97868a71fd5
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-12 01:37:32 -0300

    test: ensure util.getToken return a list of token

--- src/services/token/utils.spec.ts
+++ src/services/token/utils.spec.ts
@@ -1,5 +1,6 @@
 import validator from 'validator'
 import * as utils from './utils'
+import { promises } from 'fs'
 
 describe('Utils', () => {
   test('Should Utils.generateToken return a token AlphaNumeric and size of 16', () => {
@@ -7,5 +8,12 @@ describe('Utils', () => {
     const isAlphanumeric = validator.isAlphanumeric(token)
     expect(token.length).toBe(16)
     expect(isAlphanumeric).toBe(true)
+  })
+
+  test('Should Utils.getTokens return a array of tokens', async () => {
+    jest.spyOn(promises, 'readFile').mockReturnValueOnce(Promise.resolve('{ "tokens": ["any_token", "any_token2"]}'))
+    const { tokens } = await utils.getTokens()
+    expect(tokens[0]).toBe('any_token')
+    expect(tokens[1]).toBe('any_token2')
   })
 })
