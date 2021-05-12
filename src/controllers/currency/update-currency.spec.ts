281c1048ac8e7097e1e83437b3b9455729594d34
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-12 18:45:21 -0300

    test: ensure UpdateCurrencyController return 400 if pass a invalid currency

--- src/controllers/currency/update-currency.spec.ts
+++ src/controllers/currency/update-currency.spec.ts
@@ -1,13 +1,20 @@
 import { InvalidBodyError } from '../../controllers/errors'
-import { bodyRequestUpdate, UpdateCurrency } from '../../interfaces'
+import { bodyRequestUpdate, CurrencyValidator, UpdateCurrency } from '../../interfaces'
 import UpdateCurrencyController from './update-currency'
 
 interface SutTypes {
   sut: UpdateCurrencyController
   currencyAdapterStub: UpdateCurrency
+  currencyValidatorStub: CurrencyValidator
 }
 
 const makeSut = (): SutTypes => {
+  class CurrencyValidatorStub implements CurrencyValidator {
+    isValid (currency: string): boolean {
+      return true
+    }
+  }
+
   class CurrencyAdapterStub implements UpdateCurrency {
     async updateCurrency (body: bodyRequestUpdate): Promise<boolean> {
       return true
@@ -15,10 +22,12 @@ const makeSut = (): SutTypes => {
   }
 
   const currencyAdapterStub = new CurrencyAdapterStub()
-  const sut = new UpdateCurrencyController()
+  const currencyValidatorStub = new CurrencyValidatorStub()
+  const sut = new UpdateCurrencyController(currencyValidatorStub)
   return {
     sut,
-    currencyAdapterStub
+    currencyAdapterStub,
+    currencyValidatorStub
   }
 }
 
@@ -45,5 +54,19 @@ describe('UpdateCurrencyController', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidBodyError('Valor'))
+  })
+
+  test('Should return 400 if invalid currency is provided', async () => {
+    const { sut, currencyValidatorStub } = makeSut()
+    jest.spyOn(currencyValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        currency: 'Invalid',
+        value: 1
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidBodyError('Moeda'))
   })
 })
