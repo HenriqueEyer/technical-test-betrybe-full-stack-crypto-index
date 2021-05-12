44e846d32c4b505d0c56313b256056cd1f6d8eb6
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-12 18:27:36 -0300

    feat: ensure UpdateCurrencyController return 400 if no value is provided

--- src/controllers/currency/update-currency.ts
+++ src/controllers/currency/update-currency.ts
@@ -3,7 +3,16 @@ import { Controller, HttpRequest, HttpResponse } from '../../interfaces'
 import { InvalidBodyError } from '../../controllers/errors'
 
 export default class UpdateCurrencyController implements Controller {
-  async handle (httpRequest?: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new InvalidBodyError('Moeda'))
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['currency', 'value']
+    const messageError = {
+      currency: 'Moeda',
+      value: 'Valor'
+    }
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new InvalidBodyError(messageError[field]))
+      }
+    }
   }
 }
