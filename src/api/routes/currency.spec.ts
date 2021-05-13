d3b169fe5a892802b5bcebc3e3ed2f07d261b821
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-13 17:46:00 -0300

    test: ensure route currency return 401 if not send token

--- src/api/routes/currency.spec.ts
+++ src/api/routes/currency.spec.ts
@@ -1,12 +1,8 @@
 import request from 'supertest'
 import app from '../app'
 import * as util from '../../services/token/utils'
-import { promises } from 'fs'
 
 describe('Route Login', () => {
-  jest.spyOn(util, 'generateToken').mockReturnValueOnce('ANY_TOKEN')
-  jest.spyOn(promises, 'writeFile').mockReturnValue(Promise.resolve())
-
   test('/api/cryto/btc should return 200 and expect body', async () => {
     jest.spyOn(util, 'isValidToken').mockReturnValueOnce(Promise.resolve(true))
     await request(app)
@@ -20,6 +16,13 @@ describe('Route Login', () => {
     await request(app)
       .get('/api/cryto/btc')
       .set('Authorization', 'invalidtoken123456')
+      .expect(401)
+  })
+
+  test('/api/cryto/btc should return 401 if not send token', async () => {
+    jest.spyOn(util, 'isValidToken').mockReturnValueOnce(Promise.resolve(false))
+    await request(app)
+      .get('/api/cryto/btc')
       .expect(401)
   })
 })
