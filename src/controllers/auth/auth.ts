350653938b12b528e92a37df43e171aecf5a90eb
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-13 09:32:47 -0300

    test: ensure AuthController return false if validToken return false

--- src/controllers/auth/auth.ts
+++ src/controllers/auth/auth.ts
@@ -1,7 +1,21 @@
+import { TokenService } from '../../interfaces/token'
 import { Middleware, AuthRequest } from '../../interfaces'
 
 export default class AuthController implements Middleware {
+  private readonly tokenService: TokenService
+
+  constructor (tokenService: TokenService) {
+    this.tokenService = tokenService
+  }
+
   async handle (request: AuthRequest): Promise<boolean> {
-    return false
+    if (!request.token) {
+      return false
+    }
+    const isValid = await this.tokenService.validToken(request.token)
+    if (!isValid) {
+      return false
+    }
+    return true
   }
 }
