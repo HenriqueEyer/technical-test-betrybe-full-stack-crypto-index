dfedf8e630f90a5fd59e00cf3580ff1ee0e46b29
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-13 18:09:15 -0300

    feat: ensure post to /api/crypto/BTC return expect message and 200

--- src/api/routes/currency.ts
+++ src/api/routes/currency.ts
@@ -1,6 +1,9 @@
 import LoadCurrencyController from '../../controllers/currency/load-currency'
 import express from 'express'
 import CurrencyAdapter from '../../adapters/currency/currency'
+import UpdateCurrencyController from '../../controllers/currency/update-currency'
+import { CurrencyValidatorAdapter } from '../../services/validators/currency-validator'
+import { CurrencyValueValidatorAdapter } from '../../services/validators/currency-value-validator'
 
 const router = express.Router()
 
@@ -8,6 +11,16 @@ router.get('/', async (req, res) => {
   const GetCurrency = new CurrencyAdapter()
   const LoadCurrency = new LoadCurrencyController(GetCurrency)
   const { statusCode, body } = await LoadCurrency.handle()
+  res.status(statusCode).json(body)
+})
+
+
+router.post('/', async (req, res) => {
+  const CurrencyValidator = new CurrencyValidatorAdapter()
+  const CurrencyValueValidator = new CurrencyValueValidatorAdapter()
+  const Currency = new CurrencyAdapter()
+  const UpdateCurrency = new UpdateCurrencyController(CurrencyValidator, CurrencyValueValidator, Currency)
+  const { statusCode, body } = await UpdateCurrency.handle(req)
   res.status(statusCode).json(body)
 })
 
