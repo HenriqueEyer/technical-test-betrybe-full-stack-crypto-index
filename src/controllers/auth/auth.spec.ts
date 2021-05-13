8fb3c987d932bc6e3b54e20d98ba069322764aa5
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-13 09:33:05 -0300

    feat: ensure AuthController return false if validToken return false

--- src/controllers/auth/auth.spec.ts
+++ src/controllers/auth/auth.spec.ts
@@ -1,9 +1,53 @@
 import AuthController from './auth'
+import { TokenService } from '../../interfaces/token'
+
+interface SutTypes {
+  sut: AuthController
+  tokenServiceStub: TokenService
+}
+
+const makeSut = (): SutTypes => {
+  class TokenServiceStub implements TokenService {
+    async generateToken (password: string, email: string): Promise<string> {
+      return 'any_token'
+    }
+
+    async validToken (token: string): Promise<boolean> {
+      return Promise.resolve(true)
+    }
+  }
+
+  const tokenServiceStub = new TokenServiceStub()
+  const sut = new AuthController(tokenServiceStub)
+  return {
+    sut,
+    tokenServiceStub
+  }
+}
 
 describe('AuthController', () => {
   test('Should return false if not token is provided', async () => {
-    const sut = new AuthController()
+    const { sut } = makeSut()
     const request = {}
+    const isValid = await sut.handle(request)
+    expect(isValid).toBe(false)
+  })
+
+  test('Should return true if token is valid', async () => {
+    const { sut } = makeSut()
+    const request = {
+      token: 'valid'
+    }
+    const isValid = await sut.handle(request)
+    expect(isValid).toBe(true)
+  })
+
+  test('Should return false if token validator return false', async () => {
+    const { sut, tokenServiceStub } = makeSut()
+    jest.spyOn(tokenServiceStub, 'validToken').mockResolvedValue(Promise.resolve(false))
+    const request = {
+      token: 'invalid'
+    }
     const isValid = await sut.handle(request)
     expect(isValid).toBe(false)
   })
