a3a866f9124dc053ef050c808f2157e68c442b39
HenriqueEyer <henriqueeyer@outlook.com>
2021-05-14 12:53:07 -0300

    chore: add cors

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import Login from './routes/login'
 import currency from './routes/currency'
+import cors from 'cors'
 
 export const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({
   extended: true
